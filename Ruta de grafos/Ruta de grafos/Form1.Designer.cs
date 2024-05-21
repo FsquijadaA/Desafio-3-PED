0492520ffa596baf0f86c888d1b41fe1f4704cff
FsquijadaA <127255812+FsquijadaA@users.noreply.github.com>
2024-05-20 22:35:11 -0600

    Add files via upload

---
+++ Ruta de grafos/Ruta de grafos/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Ruta_de_grafos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInicio = new System.Windows.Forms.Label();
+            this.textBoxInicio = new System.Windows.Forms.TextBox();
+            this.buttonAnchura = new System.Windows.Forms.Button();
+            this.buttonProfundidad = new System.Windows.Forms.Button();
+            this.textBoxResultado = new System.Windows.Forms.TextBox();
+            this.panelGrafo = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // labelInicio
+            // 
+            this.labelInicio.AutoSize = true;
+            this.labelInicio.Location = new System.Drawing.Point(12, 9);
+            this.labelInicio.Name = "labelInicio";
+            this.labelInicio.Size = new System.Drawing.Size(86, 16);
+            this.labelInicio.TabIndex = 0;
+            this.labelInicio.Text = "Vértice Inicial";
+            // 
+            // textBoxInicio
+            // 
+            this.textBoxInicio.Location = new System.Drawing.Point(12, 39);
+            this.textBoxInicio.Name = "textBoxInicio";
+            this.textBoxInicio.Size = new System.Drawing.Size(100, 22);
+            this.textBoxInicio.TabIndex = 1;
+            // 
+            // buttonAnchura
+            // 
+            this.buttonAnchura.Location = new System.Drawing.Point(12, 81);
+            this.buttonAnchura.Name = "buttonAnchura";
+            this.buttonAnchura.Size = new System.Drawing.Size(208, 23);
+            this.buttonAnchura.TabIndex = 2;
+            this.buttonAnchura.Text = "Recorrer en anchura";
+            this.buttonAnchura.UseVisualStyleBackColor = true;
+            this.buttonAnchura.Click += new System.EventHandler(this.buttonAnchura_Click);
+            // 
+            // buttonProfundidad
+            // 
+            this.buttonProfundidad.Location = new System.Drawing.Point(12, 121);
+            this.buttonProfundidad.Name = "buttonProfundidad";
+            this.buttonProfundidad.Size = new System.Drawing.Size(208, 23);
+            this.buttonProfundidad.TabIndex = 3;
+            this.buttonProfundidad.Text = "Recorrer en  profundidad";
+            this.buttonProfundidad.UseVisualStyleBackColor = true;
+            this.buttonProfundidad.Click += new System.EventHandler(this.buttonProfundidad_Click);
+            // 
+            // textBoxResultado
+            // 
+            this.textBoxResultado.Location = new System.Drawing.Point(565, 39);
+            this.textBoxResultado.Multiline = true;
+            this.textBoxResultado.Name = "textBoxResultado";
+            this.textBoxResultado.ReadOnly = true;
+            this.textBoxResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResultado.Size = new System.Drawing.Size(163, 49);
+            this.textBoxResultado.TabIndex = 4;
+            // 
+            // panelGrafo
+            // 
+            this.panelGrafo.BackColor = System.Drawing.Color.White;
+            this.panelGrafo.Location = new System.Drawing.Point(12, 162);
+            this.panelGrafo.Name = "panelGrafo";
+            this.panelGrafo.Size = new System.Drawing.Size(776, 276);
+            this.panelGrafo.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelGrafo);
+            this.Controls.Add(this.textBoxResultado);
+            this.Controls.Add(this.buttonProfundidad);
+            this.Controls.Add(this.buttonAnchura);
+            this.Controls.Add(this.textBoxInicio);
+            this.Controls.Add(this.labelInicio);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInicio;
+        private System.Windows.Forms.TextBox textBoxInicio;
+        private System.Windows.Forms.Button buttonAnchura;
+        private System.Windows.Forms.Button buttonProfundidad;
+        private System.Windows.Forms.TextBox textBoxResultado;
+        private System.Windows.Forms.Panel panelGrafo;
+    }
+}
+
---
+++ Ruta de grafos/Ruta de grafos/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ruta_de_grafos
+{
+    public partial class Form1 : Form
+    {
+        private Grafo grafo;
+
+        public Form1()
+        {
+            InitializeComponent();
+            grafo = new Grafo();
+            InicializarGrafo();
+            panelGrafo.Paint += new PaintEventHandler(this.panelGrafo_Paint);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void InicializarGrafo()
+        {
+            // Crear vértices
+            Vertice santaAna = new Vertice("Santa Ana");
+            Vertice sanSalvador = new Vertice("San Salvador");
+            Vertice laLibertad = new Vertice("La Libertad");
+            Vertice sanMiguel = new Vertice("San Miguel");
+            Vertice usulutan = new Vertice("Usulután");
+
+            // Agregar vértices al grafo
+            grafo.AddVertice(santaAna);
+            grafo.AddVertice(sanSalvador);
+            grafo.AddVertice(laLibertad);
+            grafo.AddVertice(sanMiguel);
+            grafo.AddVertice(usulutan);
+
+            // Crear aristas
+            grafo.AddArista(santaAna, sanSalvador, 67);
+            grafo.AddArista(sanSalvador, laLibertad, 20);
+            grafo.AddArista(laLibertad, sanMiguel, 90);
+            grafo.AddArista(sanMiguel, usulutan, 40);
+            grafo.AddArista(usulutan, santaAna, 100);
+
+            // Opcionalmente, puedes crear aristas en ambos sentidos
+            grafo.AddArista(sanSalvador, santaAna, 67);
+            grafo.AddArista(laLibertad, sanSalvador, 20);
+            grafo.AddArista(sanMiguel, laLibertad, 90);
+            grafo.AddArista(usulutan, sanMiguel, 40);
+            grafo.AddArista(santaAna, usulutan, 100);
+        }
+
+        private void buttonAnchura_Click(object sender, EventArgs e)
+        {
+            string inicio = textBoxInicio.Text;
+            var resultado = grafo.RecorridoAnchura(new Vertice(inicio));
+            MostrarResultado(resultado);
+        }
+
+        private void buttonProfundidad_Click(object sender, EventArgs e)
+        {
+            string inicio = textBoxInicio.Text;
+            var resultado = grafo.RecorridoProfundidad(new Vertice(inicio));
+            MostrarResultado(resultado);
+        }
+
+        private void MostrarResultado(List<Vertice> resultado)
+        {
+            textBoxResultado.Clear();
+            foreach (var vertice in resultado)
+            {
+                textBoxResultado.AppendText(vertice.Nombre + Environment.NewLine);
+            }
+        }
+
+        private void panelGrafo_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            DibujarGrafo(g);
+        }
+
+        private void DibujarGrafo(Graphics g)
+        {
+            // Define posiciones fijas para los vértices para este ejemplo
+            Dictionary<string, Point> posiciones = new Dictionary<string, Point>
+            {
+                { "Santa Ana", new Point(50, 50) },
+                { "San Salvador", new Point(200, 50) },
+                { "La Libertad", new Point(200, 150) },
+                { "San Miguel", new Point(350, 150) },
+                { "Usulután", new Point(350, 250) }
+            };
+
+            // Dibujar aristas
+            foreach (var vertice in grafo.Vertices)
+            {
+                if (grafo.AdjacencyList.ContainsKey(vertice))
+                {
+                    foreach (var arista in grafo.AdjacencyList[vertice])
+                    {
+                        Point p1 = posiciones[vertice.Nombre];
+                        Point p2 = posiciones[arista.Destino.Nombre];
+                        g.DrawLine(Pens.Black, p1, p2);
+                        // Opcional: Dibujar la distancia
+                        Point midPoint = new Point((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2);
+                        g.DrawString(arista.Distancia.ToString(), this.Font, Brushes.Black, midPoint);
+                    }
+                }
+            }
+
+            // Dibujar vértices
+            foreach (var kvp in posiciones)
+            {
+                Point p = kvp.Value;
+                g.FillEllipse(Brushes.LightBlue, p.X - 10, p.Y - 10, 20, 20);
+                g.DrawEllipse(Pens.Black, p.X - 10, p.Y - 10, 20, 20);
+                g.DrawString(kvp.Key, this.Font, Brushes.Black, p.X + 10, p.Y + 10);
+            }
+        }
+    }
+
+    // Clases para el Grafo
+    public class Vertice
+    {
+        public string Nombre { get; set; }
+
+        public Vertice(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Vertice vertice && Nombre == vertice.Nombre;
+        }
+
+        public override int GetHashCode()
+        {
+            return Nombre.GetHashCode();
+        }
+    }
+
+    public class Arista
+    {
+        public Vertice Origen { get; set; }
+        public Vertice Destino { get; set; }
+        public int Distancia { get; set; }
+
+        public Arista(Vertice origen, Vertice destino, int distancia)
+        {
+            Origen = origen;
+            Destino = destino;
+            Distancia = distancia;
+        }
+    }
+
+    public class Grafo
+    {
+        private Dictionary<Vertice, List<Arista>> adjList;
+
+        public Grafo()
+        {
+            adjList = new Dictionary<Vertice, List<Arista>>();
+        }
+
+        public IEnumerable<Vertice> Vertices => adjList.Keys;
+
+        public Dictionary<Vertice, List<Arista>> AdjacencyList => adjList;
+
+        public void AddVertice(Vertice vertice)
+        {
+            if (!adjList.ContainsKey(vertice))
+            {
+                adjList[vertice] = new List<Arista>();
+            }
+        }
+
+        public void AddArista(Vertice origen, Vertice destino, int distancia)
+        {
+            if (!adjList.ContainsKey(origen))
+                AddVertice(origen);
+            if (!adjList.ContainsKey(destino))
+                AddVertice(destino);
+            adjList[origen].Add(new Arista(origen, destino, distancia));
+        }
+
+        public List<Vertice> RecorridoAnchura(Vertice inicio)
+        {
+            List<Vertice> visitados = new List<Vertice>();
+            Queue<Vertice> queue = new Queue<Vertice>();
+            queue.Enqueue(inicio);
+
+            while (queue.Count > 0)
+            {
+                Vertice actual = queue.Dequeue();
+                if (!visitados.Contains(actual))
+                {
+                    visitados.Add(actual);
+                    if (adjList.ContainsKey(actual))
+                    {
+                        foreach (Arista arista in adjList[actual])
+                        {
+                            queue.Enqueue(arista.Destino);
+                        }
+                    }
+                }
+            }
+            return visitados;
+        }
+
+        public List<Vertice> RecorridoProfundidad(Vertice inicio)
+        {
+            List<Vertice> visitados = new List<Vertice>();
+            Stack<Vertice> stack = new Stack<Vertice>();
+            stack.Push(inicio);
+
+            while (stack.Count > 0)
+            {
+                Vertice actual = stack.Pop();
+                if (!visitados.Contains(actual))
+                {
+                    visitados.Add(actual);
+                    if (adjList.ContainsKey(actual))
+                    {
+                        foreach (Arista arista in adjList[actual])
+                        {
+                            stack.Push(arista.Destino);
+                        }
+                    }
+                }
+            }
+            return visitados;
+        }
+    }
+}
